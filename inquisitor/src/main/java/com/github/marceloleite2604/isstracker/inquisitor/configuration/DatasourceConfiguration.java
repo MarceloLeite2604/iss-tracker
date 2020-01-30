313d369d550317e393ccf8eb6f72d97013d3ebb5
MarceloLeite2604 <marceloleite2604@gmail.com>
2020-01-30 13:02:34 -0300

    Adjust database password decryption

--- inquisitor/src/main/java/com/github/marceloleite2604/isstracker/inquisitor/configuration/DatasourceConfiguration.java
+++ inquisitor/src/main/java/com/github/marceloleite2604/isstracker/inquisitor/configuration/DatasourceConfiguration.java
@@ -8,6 +8,7 @@ import java.util.Map.Entry;
 import java.util.Optional;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,12 +26,23 @@ public class DatasourceConfiguration {
 		basicDataSource.setDriverClassName(databaseProperties.getDriverClassName());
 		basicDataSource.setUrl(elaborateUrl(databaseProperties));
 		basicDataSource.setUsername(databaseProperties.getUsername());
-		basicDataSource.setPassword(
-				sled.decrypt(databaseProperties.getPassword(), encryptionProperties.getKey()));
+		String password = decryptPassword(sled, databaseProperties.getPassword(),
+				encryptionProperties.getKey());
+		basicDataSource.setPassword(password);
 		basicDataSource.setMaxIdle(databaseProperties.getConnections());
 		basicDataSource.setMaxTotal(databaseProperties.getConnections());
 
 		return basicDataSource;
+	}
+
+	private String decryptPassword(Sled sled, String encryptedPassword, String key) {
+		String decryptedPassword;
+		if (StringUtils.isBlank(key)) {
+			decryptedPassword = sled.decrypt(encryptedPassword);
+		} else {
+			decryptedPassword = sled.decrypt(encryptedPassword, key);
+		}
+		return decryptedPassword;
 	}
 
 	private String elaborateUrl(DatabaseProperties databaseProperties) {
