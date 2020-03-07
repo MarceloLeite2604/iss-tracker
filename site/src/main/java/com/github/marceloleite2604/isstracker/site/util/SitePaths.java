9bb699dbed5cef994c7919255f92646dfac4f99e
MarceloLeite2604 <marceloleite2604@gmail.com>
2020-03-06 21:09:31 -0300

    Add annotation to suppress a SonarLint false positive.

--- site/src/main/java/com/github/marceloleite2604/isstracker/site/util/SitePaths.java
+++ site/src/main/java/com/github/marceloleite2604/isstracker/site/util/SitePaths.java
@@ -2,6 +2,7 @@ package com.github.marceloleite2604.isstracker.site.util;
 
 public final class SitePaths {
 
+	@SuppressWarnings("java:S1075")
 	public static final String IMAGE_FILES_ROOT_PATH = "/img/";
 	
 	private SitePaths() {
